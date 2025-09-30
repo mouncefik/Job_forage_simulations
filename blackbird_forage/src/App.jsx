e98663efc33915c41a753cc6a2693df6a43d2449
mouncef <mouncefik@gmail.com>
2025-09-30 18:26:09 +0100

    update logo

--- blackbird_forage/src/App.jsx
+++ blackbird_forage/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/logo.png'
 import './App.css'
 
 function App() {
@@ -12,11 +12,8 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Mouncef ikhoubi - Forage Blackbird simmulation</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
